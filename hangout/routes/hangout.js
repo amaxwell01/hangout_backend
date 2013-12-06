a36a370b2e2b71f217aaa4a12360e925de612444
Varun Aggarwal <varun@lc02kp4hffft0.gateway.2wire.net>
2013-12-05 20:38:44 -0800

    sending json instead of raw string

--- hangout/routes/hangout.js
+++ hangout/routes/hangout.js
@@ -3,9 +3,12 @@ var broadcastCache = {};
 
 module.exports.saveIds = function(req, res){
     broadcastCache[req.params["ho_id"]] = req.params["fb_id"];
-    res.send("OK");
+    res.json({ "status": "OK" });
 }
 
 module.exports.getIds = function(req,res){
-    res.send(broadcastCache[req.params["ho_id"]]);
+    //res.send(broadcastCache[req.params["ho_id"]]);
+    res.json({
+        "result":broadcastCache[req.params["ho_id"]]
+    });
 }
